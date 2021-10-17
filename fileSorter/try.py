61668e98a4a94732ec542eac812a54f8f956e74b
rsashna <rashna@ryerson.ca>
2021-10-16 22:51:34 -0400

    added new proj fileSorter, recompiled imgSer as imgs

---
+++ fileSorter/try.py
@@ -0,0 +1,14 @@
+import os, glob, subprocess
+
+print("program started\n\n")
+# Works ---v---
+os.system("ls")
+
+# Works --v-- lists header info
+# subprocess.run(["ls", "-l"])
+
+# Works ---v---
+# subprocess.run(["ls"])
+
+
+print("program ended\n\n")
